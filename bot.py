f43205811109270a9c15b639812b1dcfcef03179
Salbi159062 <Ev3183346@gmail.com>
2025-09-15 22:23:40 +0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+from model import idk
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+
+@bot.command()
+async def check(ctx):
+
+    if ctx.message.attachments:
+        for file in ctx.message.attachments:
+            print(type(file))
+
+            await file.save(f"./images/{file.filename}")
+            await ctx.send(f"Saved {file.filename}")
+            await ctx.send(idk(img=f"./images/{file.filename}"))
+
+    else:
+        await ctx.send("Please enter an image")
+
+    await ctx.send()
+
+bot.run("TOKEN HERE!")
