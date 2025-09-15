0f062b743beeaa164371e2d976270808c15da958
Salbi159062 <Ev3183346@gmail.com>
2025-09-15 22:27:29 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,19 @@
 import discord
 from discord.ext import commands
 from model import idk
+
+#activate intents 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
+
+#create first bot.event
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
-
+#commands
 @bot.command()
 async def hello(ctx):
     await ctx.send(f'Hi! I am a bot {bot.user}!')
@@ -21,14 +25,14 @@ async def heh(ctx, count_heh = 5):
 
 @bot.command()
 async def check(ctx):
-
-    if ctx.message.attachments:
+    
+    if ctx.message.attachments:  # - This checks if a reply with the command is a document
         for file in ctx.message.attachments:
-            print(type(file))
-
-            await file.save(f"./images/{file.filename}")
+            print(type(file))   # prints the file name on the blackboard
+            
+            await file.save(f"./images/{file.filename}") #Saves the image into the folder
             await ctx.send(f"Saved {file.filename}")
-            await ctx.send(idk(img=f"./images/{file.filename}"))
+            await ctx.send(idk(img=f"./images/{file.filename}"))  #Sends the class the document belongs to
 
     else:
         await ctx.send("Please enter an image")
